7d3ed0041300b16e31c1b8a81ca3a7f030118c7b
dmarcr1997 <dmarcr1997@gmail.com>
2020-09-15 15:33:44 -0500

    rebuild server and dependencies

--- back_end/server.js
+++ back_end/server.js
@@ -1,46 +1,37 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import dbConfig from './db.js';
+import { connect } from './utils/db.js';
 
 // Express Route
 import peopleRoute from './routes/people.route.js';
 import messageRoute from './routes/message.route.js';
 // Connecting mongoDB Database
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log('Database sucessfully connected!')
-},
-  error => {
-    console.log('Could not connect to database : ' + error)
-  }
-)
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+
+app.disable('x-powered-by');
+
 app.use(cors());
+app.use(json());
+app.use(urlencoded({extended: true}));
+
 app.use('/people', peopleRoute);
 app.use('/messages', messageRoute);
 
 // PORT
 const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log('Connected to Port ' + port);
+    })
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-// 404 Error
-app.use((req, res, next) => {
-  next(createError(404));
-});
 
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
-});
